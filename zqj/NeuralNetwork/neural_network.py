dd0e78dcadc2cf1f548b99f4197207154fc6494d
Qunjie Zhou <qunjie.zhou@tum.de>
2016-09-12 17:27:00 +0200

    implement loss with meansquare, improve code structure

--- zqj/NeuralNetwork/neural_network.py
+++ zqj/NeuralNetwork/neural_network.py
@@ -22,122 +22,77 @@ class NeuralNetwork():
         self.hiddenLayer = hiddenLayer
         self.epsilon = epsilon
         self.W = []
-        self.g1 = [0]
-        self.g2 = [0]
-        self.b1 = [0]
-        self.b2 = [0]
-        self.b3 = [0]
-        self.lossL = [0]
-        #print "Initializing a neural network with : ", len(hiddenLayer)," hidden layers, hidden layer dimension:", hiddenLayer
+        self.loss = []
+        print "Initializing a neural network with : ", len(hiddenLayer)," hidden layers, hidden layer dimension:", hiddenLayer
         
         
     def initNetwork(self, trainNum, classNum, hiddenLayer, epsilon):
         """ 
         Return:
-        - A: Activation list for each layer [a0, a1, a2]
-        - Z: A z list for each layer [0, z1, z2, z3]
-        - W: weight list for each layer  [0, w1, w2, w3]
+        - a: Activation list for each layer [a0, a1, a2]
+        - z: A z list for each layer [0, z1, z2, z3]
+        - w: weight list for each layer  [0, w1, w2, w3]
         """
-        A = [self.Xtr, epsilon*np.random.randn(hiddenLayer[0], trainNum), epsilon*np.random.randn(hiddenLayer[1], trainNum)] 
-        Z = [np.zeros((0)), epsilon*np.random.randn(hiddenLayer[0], trainNum), epsilon*np.random.randn(hiddenLayer[1], trainNum), epsilon*np.random.randn(classNum, trainNum)]
-        W = [np.zeros((0)), epsilon*np.random.randn(hiddenLayer[0], self.Xtr.shape[0]), epsilon*np.random.randn(hiddenLayer[1], hiddenLayer[0]), epsilon*np.random.randn(classNum,hiddenLayer[1])]
-         
-        return A, Z, W
+        L = len(self.hiddenLayer)
+        w = [np.zeros((0))]
+        a = [self.Xtr]
+        z = [np.zeros((0))]
 
+        for l in range(0, L):
+            w.append(epsilon*np.random.randn(hiddenLayer[l], a[l].shape[0]))
+            z.append(epsilon*np.random.randn(hiddenLayer[l], trainNum))
+            a.append(epsilon*np.random.randn(hiddenLayer[l], trainNum))
+                
+        w.append(epsilon*np.random.randn(classNum, hiddenLayer[L-1]))
+        z.append(epsilon*np.random.randn(classNum, trainNum))
+        return a, z, w
 
-    def train(self, weightConsWeight, activConsWeight, iterNum, hasLambda):
+    def train(self, weightConsWeight, activConsWeight, iterNum, hasLambda, lossType):
 
+        # Initialization 
+        # - C: number of classes, N: number of training images, L: number of layers(including output layer)
         C = self.classNum
         N = self.trainNum
         L = len(self.hiddenLayer) + 1
-        a, z, w = self.initNetwork(self.trainNum, self.classNum, self.hiddenLayer, self.epsilon)
-
-        #a = [self.Xtr]
-        #z = [0, w[1].dot(self.Xtr) ]
-
-        #a.append(self.ReLU(z[1]))
-        #z.append(w[2].dot(a[1]))
-
-        #a.append(self.ReLU(z[2]))
-        #z.append(w[3].dot(a[2]))
-
-        Lambda = np.zeros_like(z[L])
-        y = self.toHotOne(self.Ytr, C)  # Transform y to hotone representation
-
+        
         # - beta,gama: penalty coefficiencies
-        # - K: it  erations of ADMM
         beta = 1.0 * weightConsWeight 
         gamma = 1.0 * activConsWeight
+
+        # - a: activation, z: output, w: weight
+        a, z, w = self.initNetwork(self.trainNum, self.classNum, self.hiddenLayer, self.epsilon)
+        Lambda = np.zeros_like(z[L])
         
-        loss = [0,0,0,0]
+        # Transform y to hotone representation
+        y = self.toHotOne(self.Ytr, C) 
+               
         # Main part of ADMM updates
         for k in range(iterNum):
-
             # Walk through 1~L-1 layer network
             for l in range(1, L):
-                # w update solve ||Wa-z||^2
-                #w[l] = np.linalg.lstsq(a[l-1].T,z[l].T)[0].T
-                w[l] = z[l].dot(np.linalg.pinv(a[l-1]))
+                # w update
+                w[l] = np.linalg.lstsq(a[l-1].T,z[l].T)[0].T
+                #w[l] = z[l].dot(np.linalg.pinv(a[l-1]))
                
                 # a update
                 wNtr = w[l+1].T
                 a[l] = np.linalg.inv(beta * wNtr.dot(w[l+1]) + gamma * np.identity(wNtr.shape[0])).dot(beta * wNtr.dot(z[l+1]) + gamma * self.ReLU(z[l]))
                
                 # z update
-                wa = w[l].dot(a[l-1])
-                
-                # z_i < 0
-                z_s = np.copy(wa)
-                z_s[z_s > 0] = 0
-                loss_s = self.quadraCost(beta, gamma, a[l], wa, z_s) # !!
-
-                # z_i > 0 
-                z_b = (gamma * a[l] + beta * z_s) / (beta + gamma)
-                z_b[z_b < 0] = 0
-                loss_b = self.quadraCost(beta, gamma, a[l], wa, z_b)
-                
-                z_s[loss_s > loss_b] = z_b[loss_s > loss_b]
-                z[l] = np.copy(z_s)            
-            
+                z[l] = self.zUpdate(beta, gamma, w[l].dot(a[l-1]), a[l])
+                           
             # L-layer
             # w update
-            #w[L] = np.linalg.lstsq(a[L-1].T,z[L].T)[0].T            
-            w[L] = z[L].dot(np.linalg.pinv(a[L-1]))
+            w[L] = np.linalg.lstsq(a[L-1].T,z[L].T)[0].T            
+            #w[L] = z[L].dot(np.linalg.pinv(a[L-1]))
             
-            # z update 
-            waL = w[L].dot(a[L-1])
-            zL = np.zeros(waL.shape)
-
-            # y_i = 1
-            # zi > 1
-            zL_b = np.copy(waL) - Lambda / (2 * beta)
-            zL_b[zL_b < 1] = 1
-            lossL_b = self.outputCost(beta, waL, zL_b, y, 1, Lambda)
-            
-            # zi < 1
-            zL_s = waL +  (1 - Lambda) / (2 * beta)
-            zL_s[zL_s > 1] = 1
-            lossL_s = self.outputCost(beta, waL, zL_s, y, 1, Lambda)
-            
-            zL_s[lossL_s > lossL_b] = zL_b[lossL_s > lossL_b]
-            zL[y == 1] = zL_s[y == 1]
-
-            # y_i = 0
-            # zi < 0
-            zL_s = np.copy(waL) - Lambda / (2 * beta)
-            zL_s[zL_s > 0] = 0
-            lossL_s = self.outputCost(beta, waL, zL_s, y, 0, Lambda)
-
-            # zi > 0
-            zL_b = waL - (1 + Lambda) / (2 * beta)
-            zL_b[zL_b < 0] = 0
-            lossL_b = self.outputCost(beta, waL, zL_b, y, 0, Lambda)
-                
-            zL_s[lossL_s > lossL_b] = zL_b[lossL_s > lossL_b]
-            zL[y == 0] = zL_s[y == 0] 
-            
-            # Update zL
-            z[L] = zL
+            # zL update
+            waL =  w[L].dot(a[L-1])
+            """ lossType: hinge, msq, smx """
+            zLastUpdateOpt = {'hinge': self.zLastUpdateWithHinge, 'msq': self.zLastUpdateWithMeanSq }
+            z[L] = zLastUpdateOpt[lossType](beta, waL, y, Lambda)
+            #z[L] = self.zLastUpdateWithHinge(beta, w[L].dot(a[L-1]), y, Lambda)  
+            #z[L] = self.zLastUpdateWithMeanSq(beta, waL, y, Lambda)
 
             # lambda update
             if hasLambda:
@@ -147,21 +102,13 @@ class NeuralNetwork():
             beta *= 1.05
             gamma *= 1.05
             
-            """ DEBUG: loss
-            self.g1.append(np.sum(gamma * (a[1] - self.ReLU(z[1])) ** 2))
-            self.g2.append(np.sum(gamma * (a[2] - self.ReLU(z[2])) ** 2))
-            self.b1.append(np.sum(beta * (z[1] - w[1].dot(a[0])) ** 2))
-            self.b2.append(np.sum(beta * (z[2] - w[2].dot(a[1])) ** 2))
-            self.b3.append(np.sum(beta * (z[L] - w[L].dot(a[L-1])) ** 2))
-            """
-            
-            
+            # Calculate total loss
+            self.loss.append(self.totalLoss(beta, gamma, a, z, w, y, Lambda))
+        
         # Save the W to network
         self.W = w
+        # print self.loss
 
-        
-
-        
     def predict(self, Xte):
         """
         Inputs:
@@ -171,10 +118,66 @@ class NeuralNetwork():
         - Yte: A numpy array containing predicted labels of input images
         """
         w = self.W
-        y = w[3].dot(self.ReLU(w[2].dot(self.ReLU(w[1].dot(Xte)))))
+        z = w[1].dot(Xte)
+        for l in range(1,len(self.hiddenLayer)+1):
+            z = w[l+1].dot(self.ReLU(z))
+            #y = w[3].dot(self.ReLU(w[2].dot(self.ReLU(z))))
+        y = z
         #print y[:, xrange(2)]
         return  np.argmax(y, axis=0)
 
+   
+    def zUpdate(self, beta, gamma, wa, al):
+        # z_i < 0
+        z_s = np.copy(wa)
+        z_s[z_s > 0] = 0
+        loss_s = self.quadraCost(beta, gamma, al, wa, z_s) # !!
+
+        # z_i > 0 
+        z_b = (gamma * al + beta * z_s) / (beta + gamma)
+        z_b[z_b < 0] = 0
+        loss_b = self.quadraCost(beta, gamma, al, wa, z_b)
+        
+        z_s[loss_s > loss_b] = z_b[loss_s > loss_b]
+        
+        return np.copy(z_s)
+    
+    def zLastUpdateWithHinge(self, beta, waL, y, Lambda):
+        zL = np.zeros(waL.shape)
+        # y_i = 1
+        # zi > 1
+        zL_b = np.copy(waL) - Lambda / (2 * beta)
+        zL_b[zL_b < 1] = 1
+        lossL_b = self.outputCost(beta, waL, zL_b, y, 1, Lambda)
+        
+        # zi < 1
+        zL_s = waL +  (1 - Lambda) / (2 * beta)
+        zL_s[zL_s > 1] = 1
+        lossL_s = self.outputCost(beta, waL, zL_s, y, 1, Lambda)
+        
+        zL_s[lossL_s > lossL_b] = zL_b[lossL_s > lossL_b]
+        zL[y == 1] = zL_s[y == 1]
+
+        # y_i = 0
+        # zi < 0
+        zL_s = np.copy(waL) - Lambda / (2 * beta)
+        zL_s[zL_s > 0] = 0
+        lossL_s = self.outputCost(beta, waL, zL_s, y, 0, Lambda)
+
+        # zi > 0
+        zL_b = waL - (1 + Lambda) / (2 * beta)
+        zL_b[zL_b < 0] = 0
+        lossL_b = self.outputCost(beta, waL, zL_b, y, 0, Lambda)
+            
+        zL_s[lossL_s > lossL_b] = zL_b[lossL_s > lossL_b]
+        zL[y == 0] = zL_s[y == 0] 
+        return zL
+    
+    def zLastUpdateWithMeanSq(self, beta, waL, y, Lambda):
+        return (y + beta * waL)/ (1 + beta) - 0.5 * Lambda / (1 + beta)
+
+    def meanSqrLoss(self, z, y):
+        return np.sum(z - y)
 
     def toHotOne(self, Y, C):
         """ Construct Hot-one representation of Y """
@@ -184,14 +187,26 @@ class NeuralNetwork():
         return y
 
 
-    def outputCost(self, beta, wa, z, y, isOne, Lambda):
-        return self.hingeLoss(z, y, isOne) + beta * (z - wa) ** 2 + z * Lambda
+    def totalLoss(self, beta, gamma, a, z, w, y, Lambda):
+        L = len(self.hiddenLayer) + 1
+        dataLoss = np.sum(self.hinge(z[L], y))
+        quadraLoss = np.sum(self.zQuadraLoss(beta, z[L], w[L].dot(a[L-1])))
+        lagraLoss = np.sum(z[L] * Lambda)
+        for l in range(1,L):
+            quadraLoss += np.sum(self.quadraCost(beta, gamma, a[l], w[l].dot(a[l-1]), z[l]))
+        return dataLoss + quadraLoss + lagraLoss
 
+    def outputCost(self, beta, wa, z, y, isOne, Lambda): # can be improved
+        return self.hingeLoss(z, y, isOne) + self.zQuadraLoss(beta, wa, z) + z * Lambda
 
     def quadraCost(self, beta, gamma, a, wa, z):
-        """ Calculate elementwise cost """
-        return  gamma * (a - self.ReLU(z)) ** 2 + beta * (z - wa) ** 2 
-            
+        return  self.aQuadraLoss(gamma, a, z) + self.zQuadraLoss(beta, wa, z)
+
+    def zQuadraLoss(self, beta, wa, z):
+        return beta * (z - wa) ** 2
+    
+    def aQuadraLoss(self, gamma, a, z):
+        return gamma * (a - self.ReLU(z)) ** 2 
     
     def ReLU(self, x):
         """ Evaluate Rectified Linear Unit """
@@ -199,8 +214,13 @@ class NeuralNetwork():
         xn[xn < 0] = 0
         return xn
 
-
-    def hingeLoss(self, z, y, isOne):
+    def hinge(self, z, y): # to replace the old one
+        loss = np.zeros(y.shape)
+        loss[y == 0] = np.maximum(z,loss)[y == 0]
+        loss[y == 1] = np.maximum(1-z,loss)[y == 1]
+        return loss
+    
+    def hingeLoss(self, z, y, isOne): # can be improved
         """ Evaluate Hinge Loss """
         zn = np.copy(z)
         x = np.zeros(z.shape)
@@ -208,18 +228,8 @@ class NeuralNetwork():
             zn[y == 0] = np.maximum(zn,x)[y == 0]
         else:
             zn[y == 1] = np.maximum(1-zn,x)[y == 1]
-
-        self.lossL.append(np.sum(zn))
         return zn
 
-
-    def toEvenOddLabel(self, Y, C):
-        return Y % 2
-    
-    def toBinary(self, Y):
-        yb = Y % 2
-        return self.toHotOne(yb, 2)
-    
     def multiSVMLoss(self, Spre,Ytrue):
         """ Evaluate Multiclass SVM Loss """        
         pass  
