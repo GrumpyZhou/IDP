4ca6d1db85a245c308c7004010691e2eb45370e7
Qunjie Zhou <qunjie.zhou@tum.de>
2016-11-19 10:18:39 +0100

    add dataset wrapper

--- zqj/NeuralNetwork/data_utils.py
+++ zqj/NeuralNetwork/data_utils.py
@@ -6,34 +6,100 @@ def getMnistData(path):
     
     Xtr,Ytr = read("training",path)
     Xte,Yte = read("testing",path)
-    Xtr.astype("float")
-    Ytr.astype("float")
-    Xte.astype("float")
-    Yte.astype("float") 
+    Xtr = Xtr.astype(np.float64)
+    Ytr = Ytr.astype(np.int64)
+    Xte = Xte.astype(np.float64)
+    Yte = Yte.astype(np.int64)
 
     return Xtr,Ytr,Xte,Yte
 
 def read(dataset = "training", path="."):
 
     if dataset is "training":
-        fname_img = os.path.join(path, 'train-images-idx3-ubyte')
-        fname_lbl = os.path.join(path, 'train-labels-idx1-ubyte')
+        fnameImg = os.path.join(path, 'train-images-idx3-ubyte')
+        fnameLbl = os.path.join(path, 'train-labels-idx1-ubyte')
     elif dataset is "testing":
-        fname_img = os.path.join(path, 't10k-images-idx3-ubyte')
-        fname_lbl = os.path.join(path, 't10k-labels-idx1-ubyte')
+        fnameImg = os.path.join(path, 't10k-images-idx3-ubyte')
+        fnameLbl = os.path.join(path, 't10k-labels-idx1-ubyte')
     else:
         raise ValueError, "dataset must be 'testing' or 'training'"
 
     # Load everything in some numpy arrays
-    with open(fname_lbl, 'rb') as flbl:
+    with open(fnameLbl, 'rb') as flbl:
         magic, num = struct.unpack(">II", flbl.read(8))
         lbl = np.fromfile(flbl, dtype=np.int8)
 
-    with open(fname_img, 'rb') as fimg:
+    with open(fnameImg, 'rb') as fimg:
         magic, num, rows, cols = struct.unpack(">IIII", fimg.read(16))
         img = np.fromfile(fimg, dtype=np.uint8).reshape(len(lbl), rows, cols)
-
     return img,lbl
+
+
+def getMnistDataSets(path, valSize = 0, reshape=True, transpose=True):
+    
+    Xtr,Ytr = read("training",path)
+    Xte,Yte = read("testing",path)
+    Xtr = Xtr.astype(np.float64)
+    Ytr = Ytr.astype(np.int64)
+    Xte = Xte.astype(np.float64)
+    Yte = Yte.astype(np.int64)
+    
+    train = DataSet(Xtr[valSize:], Ytr[valSize:], reshape=reshape, transpose=transpose)
+    test = DataSet(Xte, Yte, reshape=reshape, transpose=transpose)
+    if valSize != 0:
+        validation = DataSet(Xtr[:valSize], Ytr[:valSize], reshape=reshape, transpose=transpose)
+        return {"train": train, "validation": validation, "test": test}
+    return {"train": train, "test": test}
+    
+class DataSet():
+
+    def __init__(self, images, labels, reshape=True, transpose=True):
+        self.imgNum = images.shape[0]
+        
+        if reshape:
+            images = np.reshape(images, (images.shape[0], -1))
+        if transpose:
+            images = images.T
+            print images.shape
+            
+        self.images = images
+        self.labels = labels
+        self.round = 0
+        self.index= 0
+        
+    @property
+    def images(self):
+        return self.images
+    @property
+    def labels(self):
+        return self.labels
+    @property
+    def imgNum(self):
+        return self.imgNum
+    @property
+    def round(self):
+        return self.round
+
+    def nextBatch(self, batchSize):
+        """Return the next 'batchSize' entries from this data set."""
+        start = self.index
+        self.index += batch_size
+        print start, self.index, self.imgNum
+        if self.index > self.imgNum:
+            # One round completed
+            self.round += 1
+            # Shuffle the data
+            perm = numpy.arange(self.imgNum)
+            numpy.random.shuffle(perm)
+            self.images = self.images[perm]
+            self.labels = self.labels[perm]
+            # Start next round
+            print 'Start round #',self.round
+            start = 0
+            self.index = batchSize
+            assert batch_size <= self._num_examples
+        end = self.index
+    return self.images[start:end], self.labels[start:end]
 
 def show(image):
     """
