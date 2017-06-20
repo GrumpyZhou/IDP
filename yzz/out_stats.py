fe82648261699c0d72c01954738e73b3259bc4af
Qunjie Zhou <qunjie.zhou@tum.de>
2017-06-20 20:53:02 +0200

    Remove Zhenzhang part

--- yzz/out_stats.py
+++
@@ -1,8 +0,0 @@
-import pstats
-
-p = pstats.Stats('main.profile')
-p.strip_dirs().sort_stats("cumtime")
-p.print_stats(20)
-
-print "=====================  Callers ===================="
-p.print_callers(10)
--- yzz/profile.sh
+++
@@ -1,9 +0,0 @@
-#bin/bash
-# Generate profile
-python -m cProfile -o main.profile mnist.py
-
-# Convert .profile into .txt 
-python out_stats.py > stats.txt
-
-cat stats.txt
-rm main.profile
